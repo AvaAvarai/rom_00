8d838c7e6bad9d00d4fa5712dccf9f423fbd9a1b
AvaAvarai <74805301+AvaAvarai@users.noreply.github.com>
2021-01-24 14:45:43 -0800

    Display loading.png image

--- src/common.h
+++ src/common.h
@@ -1,2 +1,8 @@
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
 #include <stdio.h>
+
+#define WIDTH 640
+#define HEIGHT 480
+
+#define LOAD_SCREEN_PATH "loading.png"
--- src/main.c
+++ src/main.c
@@ -3,16 +3,26 @@
 int main(int argc, char* argv[]) {
 
     SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Texture *img;
+    int w, h;
+
     SDL_Init(SDL_INIT_VIDEO);
 
     window = SDL_CreateWindow(
         "An SDL2 window",                  // window title
         SDL_WINDOWPOS_UNDEFINED,           // initial x position
         SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
+        WIDTH,                               // width, in pixels
+        HEIGHT,                               // height, in pixels
         SDL_WINDOW_OPENGL                  // flags
     );
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    img = IMG_LoadTexture(renderer, LOAD_SCREEN_PATH);
+    SDL_QueryTexture(img, NULL, NULL, &w, &h);
+    SDL_Rect texr; texr.x = WIDTH/2 - w/2; texr.y = HEIGHT/2 - h/2; texr.w = w; texr.h = h;
 
     if (window == NULL) {
         printf("Could not create window: %s\n", SDL_GetError());
@@ -23,16 +33,20 @@ int main(int argc, char* argv[]) {
     while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
-            switch (event.type)
-            {
-            case SDL_KEYDOWN:
-                running = 0;
-                break;
-            
-            default:
-                break;
+            switch (event.type) {
+                case SDL_KEYDOWN:
+                    running = 0;
+                    break;
+                default:
+                    break;
             }
         }
+
+        SDL_RenderClear(renderer);
+
+        SDL_RenderCopy(renderer, img, NULL, &texr);
+
+        SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyWindow(window);
